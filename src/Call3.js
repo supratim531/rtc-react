be80a8764f9d88c54444026ed57725a19fa5c57b
supratim531 <91423964+supratim531@users.noreply.github.com>
2023-07-29 14:01:19 +0530

    Update Call3.js

--- src/Call3.js
+++ src/Call3.js
@@ -1,7 +1,8 @@
 import io from 'socket.io-client';
 import React, { useEffect, useMemo, useState } from 'react';
 
-const socket = io.connect('http://localhost:8888');
+// const socket = io.connect('http://localhost:8888');
+const socket = io.connect('https://rtc-node.onrender.com/');
 // const socket = io('https://c2df-49-37-9-76.ngrok-free.app', {
 //   autoConnect: true,
 //   reconnection: true,
